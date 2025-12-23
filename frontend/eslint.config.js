4c39672a3541570bf580757e7022a8b62e8e1541
Thibaut <thibaut.deveraux@gmail.com>
2025-12-23 23:23:39 +0100

    Create eslint.config.js

---
+++ frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+);
