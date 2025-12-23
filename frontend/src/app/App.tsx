41be22417fcdf57a6b52d3a9bd1834f2a68c7935
Thibaut <thibaut.deveraux@gmail.com>
2025-12-23 23:41:57 +0100

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./routes";
+import { Layout } from "../components/Layout";
 
-export default function App() {
-  return <RouterProvider router={router} />;
+export function App() {
+  return (
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  );
 }
