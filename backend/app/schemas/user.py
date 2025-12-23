674d6afc381fcea33bfe160ee36e4fafd8151119
Thibaut <thibaut.deveraux@gmail.com>
2025-12-24 00:13:52 +0100

    Create user.py

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,14 @@
+from fastapi_users import schemas
+from uuid import UUID
+
+
+class UserRead(schemas.BaseUser[UUID]):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
